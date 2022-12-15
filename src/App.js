f8c204c6a512f288a1bcc760664213009bbd1c44
thanh711 <thanhsonhoc00@gmail.com>
2022-12-15 22:14:26 +0700

    dan layout

--- src/App.js
+++ src/App.js
@@ -12,13 +12,18 @@ import "antd/dist/antd.css";
 import LogIn from "./Info/Account/LogIn";
 import Register from "./Info/Account/Register";
 import TrangChu from "./templates/trangChu/TrangChu";
+import IndexCyberbugs from "./components/Cyberbugs/IndexCyberbugs";
+import CreateProject from "./pages/CyberBugs/CreateProject/CreateProject";
 
 function App() {
   return (
     <div className="App">
       <HistoryRouter history={history}>
         <Routes>
-          <Route path="/" element={<TrangChu />} />
+          <Route path="/" element={<TrangChu />}>
+            <Route path="/" element={<IndexCyberbugs />} />
+            <Route path="/reateproject" element={<CreateProject />} />
+          </Route>
         </Routes>
       </HistoryRouter>
     </div>
